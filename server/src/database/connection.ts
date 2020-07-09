b1bf89287051abaaebf8a67e9cb7d3e18ff19e10
MarcosJBM <marcosjbotene@gmail.com>
2020-07-09 20:14:48 -0300

    Configurando conexão com o banco de dados.

---
+++ server/src/database/connection.ts
@@ -0,0 +1,12 @@
+import knex from 'knex';
+import path from 'path';
+
+const connection = knex({
+    client: 'sqlite3', //Nome do Banco de Dados
+    connection: {
+        //Conexão com o arquivo onde ficara guardado os dados.
+        filename: path.resolve(__dirname, 'database.sqlite'),
+    },
+});
+
+export default connection;
---
+++ server/src/routes.ts
@@ -0,0 +1,9 @@
+import express, { request, response } from "express";
+
+const routes = express.Router();
+
+routes.get("/users", (request, response) => {
+    return response.json({ message: 'Hello World' });
+});
+
+export default routes;
