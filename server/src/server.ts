d8aac7660c3d6bf05eff6dc2ebbdf9d1602da67d
Marcos Botene <marcosjbotene@gmail.com>
2020-07-08 22:19:16 -0300

    Metodos HTTP adicionados

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,40 @@
-import express from 'express';
+import express, { request, response } from 'express';
 
 const app = express();
 
+app.use(express.json());
+
+const users = [
+    'Marcos',
+    'Daniel',
+    'Helen'
+];
+
 app.get('/users', (request, response) => {
-    return console.log('Listagem de Usuarios');
+    const search = String(request.query.search);
+
+    const filteredUsers = search ? users.filter(user => user.includes(search)) : users;
+
+    return response.json(filteredUsers);
+});
+
+app.get('/users/:id', (request, response) => {
+    const id = Number(request.params.id);
+
+    const user = users[id];
+
+    return response.json(user);
+});
+
+app.post('/users', (request, response) => {
+    const data = request.body;
+
+    const user = {
+        name: data.name,
+        email: data.email,
+    };
+
+    return response.json(user);
 });
 
 app.listen(3333);
