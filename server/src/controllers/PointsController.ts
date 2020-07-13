ba1ea408918b0270390061b47f034c1e00cf9512
MarcosJBM <marcosjbotene@gmail.com>
2020-07-13 20:28:31 -0300

    Listar ponto de Coleta Filtrado.

--- server/src/controllers/PointsController.ts
+++ server/src/controllers/PointsController.ts
@@ -1,7 +1,25 @@
-import { Request, Response } from 'express';
+import { Request, Response, query } from 'express';
 import knex from '../database/connection';
 
 class PointsController {
+    async index(request: Request, response: Response) {
+        const { city, uf, items } = request.query;
+
+        const parsedItems = String(items)
+            .split(',')
+            .map(item => Number(item.trim()));
+
+        const points = await knex('points')
+            .join('point_items', 'points.id', '=', 'point_items.point_id')
+            .whereIn('point_items.item_id', parsedItems)
+            .where('city', String(city))
+            .where('uf', String(uf))
+            .distinct()
+            .select('points.*');
+
+        return response.json(points);
+    };
+
     //Retorna um unico Registro.
     async show(request: Request, response: Response) {
         const { id } = request.params;
@@ -16,11 +34,13 @@ class PointsController {
         //Relaciona os ITEMS com um PONTO em pelo ID.
         const items = await knex('items')
             .join('point_items', 'items.id', '=', 'point_items.item_id')
-            .where('point_items.point_id', id);
+            .where('point_items.point_id', id)
+            .select('items.title');
 
         return response.json({ point, items });
     };
 
+    //Cria um ponto de Coleta.
     async create(request: Request, response: Response) {
         const {
             name,
@@ -61,6 +81,8 @@ class PointsController {
 
         //Relacionando o ID do PONTO com os IDs dos ITEMS.
         await trx('point_items').insert(pointItems);
+
+        await trx.commit();
 
         return response.json({
             id: point_id,
