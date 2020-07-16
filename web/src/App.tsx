a656c6b35bad9eb6e22ec8c9a2f577d364a2c814
MarcosJBM <marcosjbotene@gmail.com>
2020-07-15 21:48:18 -0300

    Criação da página Home.

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import React from "react";
+
+import './App.css';
+
+import Home from "./pages/Home";
 
 const App = () => {
   return (
-    <h1>Hello World</h1>
-  );
+    <Home />
+  )
 };
 
 export default App;
