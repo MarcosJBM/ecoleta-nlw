a9c7c81756ce0317a68157413b2a9978b96e653f
MarcosJBM <marcosjbotene@gmail.com>
2020-08-13 23:21:05 -0300

    Atualizando estilização.

--- mobile/src/pages/Detail/index.tsx
+++ mobile/src/pages/Detail/index.tsx
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import { Feather as Icon, FontAwesome } from '@expo/vector-icons';
 import { useNavigation, useRoute } from '@react-navigation/native';
-import { View, Text, StyleSheet, TouchableOpacity, Image, SafeAreaView, Linking } from 'react-native';
+import { View, Text, TouchableOpacity, Image, SafeAreaView, Linking } from 'react-native';
 import { RectButton } from 'react-native-gesture-handler';
 import api from '../../services/api';
 import * as MailComposer from 'expo-mail-composer';
+
+import styles from './styles';
 
 interface Params {
     point_id: number;
@@ -91,79 +93,5 @@ const Detail = () => {
         </SafeAreaView>
     );
 };
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        paddingHorizontal: 32,
-        paddingTop: 18,
-    },
-
-    pointImage: {
-        width: '100%',
-        height: 120,
-        resizeMode: 'cover',
-        borderRadius: 10,
-        marginTop: 28,
-    },
-
-    pointName: {
-        color: '#322153',
-        fontSize: 28,
-        fontFamily: 'Ubuntu_700Bold',
-        marginTop: 24,
-    },
-
-    pointItems: {
-        fontFamily: 'Roboto_400Regular',
-        fontSize: 16,
-        lineHeight: 24,
-        marginTop: 8,
-        color: '#6C6C80'
-    },
-
-    address: {
-        marginTop: 32,
-    },
-
-    addressTitle: {
-        color: '#322153',
-        fontFamily: 'Roboto_500Medium',
-        fontSize: 16,
-    },
-
-    addressContent: {
-        fontFamily: 'Roboto_400Regular',
-        lineHeight: 24,
-        marginTop: 8,
-        color: '#6C6C80'
-    },
-
-    footer: {
-        borderTopWidth: StyleSheet.hairlineWidth,
-        borderColor: '#999',
-        paddingVertical: 20,
-        paddingHorizontal: 32,
-        flexDirection: 'row',
-        justifyContent: 'space-between'
-    },
-
-    button: {
-        width: '48%',
-        backgroundColor: '#34CB79',
-        borderRadius: 10,
-        height: 50,
-        flexDirection: 'row',
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-
-    buttonText: {
-        marginLeft: 8,
-        color: '#FFF',
-        fontSize: 16,
-        fontFamily: 'Roboto_500Medium',
-    },
-});
 
 export default Detail;
