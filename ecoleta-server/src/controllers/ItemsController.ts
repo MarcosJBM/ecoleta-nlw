32e98c901563973502f9721444778095bf83d009
Marcos Botene <marcosjbotene@gmail.com>
2021-05-09 19:20:17 -0300

    Created: types created for Items.

--- ecoleta-server/src/controllers/ItemsController.ts
+++ ecoleta-server/src/controllers/ItemsController.ts
@@ -1,10 +1,12 @@
 import { Request, Response } from 'express';
 
+import { Items } from '../@types';
+
 import { connection } from '../database/connection';
 
 export default {
   async index(request: Request, response: Response) {
-    const items = await connection('items').select('*');
+    const items: Items[] = await connection('items').select('*');
 
     const serializedItems = items.map(item => {
       return {
@@ -14,6 +16,6 @@ export default {
       };
     });
 
-    return response.json(serializedItems);
+    return response.status(200).json(serializedItems);
   },
 };
