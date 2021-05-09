a7b3cbaca0640f1ba1319e9418dc27460694f032
Marcos Botene <marcosjbotene@gmail.com>
2021-05-09 18:02:45 -0300

    Fixed: API is not running.

--- ecoleta-server/src/routes.ts
+++ ecoleta-server/src/routes.ts
@@ -1,11 +1,11 @@
-import express from "express";
-import { celebrate, Joi } from "celebrate";
+import express from 'express';
+import { celebrate, Joi } from 'celebrate';
 
-import multer from "multer";
-import multerConfig from "./config/multer";
+import multer from 'multer';
+import multerConfig from './config/multer';
 
-import PointsController from "./controllers/PointsController";
-import ItemsController from "./controllers/ItemsController";
+import PointsController from './controllers/PointsController';
+import ItemsController from './controllers/ItemsController';
 
 const routes = express.Router();
 const upload = multer(multerConfig);
@@ -13,13 +13,13 @@ const upload = multer(multerConfig);
 const pointsController = new PointsController();
 const itemsController = new ItemsController();
 
-routes.get("/items", itemsController.index);
+routes.get('/items', itemsController.index);
 
-routes.get("/points", pointsController.index);
-routes.get("/points/:id", pointsController.show);
+routes.get('/points', pointsController.index);
+routes.get('/points/:id', pointsController.show);
 routes.post(
-  "/points",
-  upload.single("image"),
+  '/points',
+  upload.single('image'),
   celebrate(
     {
       body: Joi.object().keys({
