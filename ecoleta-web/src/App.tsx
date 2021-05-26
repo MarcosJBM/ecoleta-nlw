28060e75b77b88cbc425290e1e35e625e0baa496
Marcos Botene <marcosjbotene@gmail.com>
2021-05-25 22:34:22 -0300

    Created: global styles using Styled Components.

--- ecoleta-web/src/App.tsx
+++ ecoleta-web/src/App.tsx
@@ -3,13 +3,14 @@ import { Route, BrowserRouter } from 'react-router-dom';
 import { Home } from '../src/pages/Home';
 import { CreatePoint } from '../src/pages/CreatePoint';
 
-import './App.css';
+import GlobalStyles from './styles/GlobalStyles';
 
 export const App = () => {
   return (
     <BrowserRouter>
       <Route exact path={'/'} component={Home} />
       <Route path={'/create-point'} component={CreatePoint} />
+      <GlobalStyles />
     </BrowserRouter>
   );
 };
