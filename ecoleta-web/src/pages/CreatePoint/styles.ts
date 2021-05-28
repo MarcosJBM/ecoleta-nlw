35664c3e0252d5a4c9aa5af5bab5d6f501bbd4c2
Marcos Botene <marcosjbotene@gmail.com>
2021-05-27 23:44:01 -0300

    Created: ItemsGrid component for Create Point Page.

--- ecoleta-web/src/pages/CreatePoint/styles.ts
+++ ecoleta-web/src/pages/CreatePoint/styles.ts
@@ -133,3 +133,41 @@ export const Field = styled.div`
     color: #6c6c80;
   }
 `;
+
+export const ItemsGrid = styled.ul`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 16px;
+  list-style: none;
+
+  > li {
+    background: #f5f5f5;
+    border: 2px solid #f5f5f5;
+    height: 180px;
+    border-radius: 8px;
+    padding: 32px 24px 16px;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+
+    text-align: center;
+
+    cursor: pointer;
+
+    &.selected {
+      background: #e1faec;
+      border: 2px solid #34cb79;
+    }
+
+    > span {
+      flex: 1;
+      margin-top: 12px;
+
+      display: flex;
+      align-items: center;
+      color: var(--title-color);
+    }
+  }
+`;
