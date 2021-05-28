d8d7e9a321a589a2c5cc2eb150821763e1b25c16
Marcos Botene <marcosjbotene@gmail.com>
2021-05-27 23:32:14 -0300

    Created: Header component for Create Point Page.

--- ecoleta-web/src/pages/CreatePoint/styles.ts
+++ ecoleta-web/src/pages/CreatePoint/styles.ts
@@ -6,3 +6,25 @@ export const Container = styled.div`
 
   margin: 0 auto;
 `;
+
+export const Header = styled.header`
+  margin-top: 48px;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  > a {
+    color: var(--title-color);
+    font-weight: bold;
+    text-decoration: none;
+
+    display: flex;
+    align-items: center;
+
+    > svg {
+      margin-right: 16px;
+      color: var(--primary-color);
+    }
+  }
+`;
